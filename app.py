e5adfa11ea565ed3880a0810bbff203b1cb7b645
sharky-2 <blazmedvode@gmail.com>
2025-03-19 02:30:00 +0100

    Flask modules added

--- app.py
+++ app.py
@@ -9,23 +9,29 @@ def MainPage(): return render_template("index.html")
 
 @app.route("/Test") 
 def Test(): 
+    name_ = "test-page"
     module = ""
 
     with open("data/content.json") as f:
         data = json.load(f)
 
-    for char in data.get("testLibary2", []): 
+    for char in data.get(name_, []): 
         type_ = char.get("type", None)
         title = char.get("title", None)
         text = char.get("text", None)
         img = char.get("img", None)
         button = char.get("button", None)
 
+        accordion_amount = char.get("accordion_amount", None)
+        image_amount = char.get("image_amount", None)
+        blog_amount = char.get("blog_amount", None)
+        card_amount = char.get("card_amount", None)
+        
         module_function = getattr(data_utils, type_, None)
         if module_function: 
-            module += module_function(title, text, img, button)
+            module += module_function(title, text, img, button, accordion_amount, image_amount, blog_amount, card_amount, name_)
 
     return render_template("test.html", modules = module)
 
 if __name__ == '__main__':
-    app.run(debug=True)a
+    app.run(debug=True)
--- data/data_utils.py
+++ data/data_utils.py
@@ -1,10 +1,161 @@
+import json
 
-def TitleCard(title, text, img, button):
+def design(title, text, img, button, accordion_amount, image_amount, blog_amount, card_amount, page_name):
     module = f"""
-    <div class="TextFrames">
-        <h1 class="TitleText TitleMainAnim">{title}</h1>
-        <label class="UnderTitleText TextMainAnim">{text}</label>
-        <a href="#" class="ButtonDesign ButtonHoverDesign ButtonMainAnim"><input type="button" value="{button}" class="InputButtonDesign"></a>
-    </div>
+    """
+    return module
+
+def TextModule(title, text, img, button, accordion_amount, image_amount, blog_amount, card_amount, page_name):
+    module = f"""
+    <section>
+        <div></div>
+        <div><label class="code-text-design">text-4xl text-white tracking-tighter text-balance</label></div>
+        <div><h1 class="title-design-2">{title}</h1></div>
+        <div><label class="code-text-design">text-base text-white</label></div>
+        <div><label class="text-design-2">{text}</label></div>
+        <div><button class="button-design-2 hover-glitchy">Discover Stellar Plus</button></div>
+    
+        <code class="side-frame-text blue-color code-text-design-2">introduction</code>
+    </section> 
+    """
+    return module
+
+def ImageModule(title, text, img, button, accordion_amount, image_amount, blog_amount, card_amount, page_name):
+    module = f"""
+    <!-- Img Module -->
+    <section>
+        <div></div>
+        <div><label class="code-text-design">text-4xl text-white tracking-tighter text-balance</label></div>
+        <div><h1 class="title-design-2">{title}</h1></div>
+        <div><img src="{img}" width="100%"></div>
+    </section>
+    """
+    return module
+
+def AccordionModule(title, text, img, button, accordion_amount, image_amount, blog_amount, card_amount, page_name):
+    item = ""
+    for i in range(accordion_amount):
+
+        with open("data/content.json") as f:
+            data = json.load(f)
+
+        for char in data.get(page_name, []):
+            type_ = char.get("type", None)
+            if type_ == "AccordionModule": 
+
+                question = char.get(f"question-{i}", None)
+                answer = char.get(f"answer-{i}", None)
+                    
+                item += f"""
+                <div class="item text-design-2">
+                    <div class="question">
+                        <label class="text">{question}</label>
+                        <div class="dropdown">></div>
+                    </div>
+                    <div class="answer">{answer}</div>
+                </div>
+                """
+
+    module = f"""
+    <section>
+        <div></div>
+        <div style="align-items: start; padding: 0;">
+            <div id="accordion-list">
+                {item}
+            </div>
+        </div>
+        <code class="side-frame-text pink-color code-text-design-2">Accordions</code>
+    </section>
+    """
+    return module
+
+def ImageGalreyModule(title, text, img, button, accordion_amount, image_amount, blog_amount, card_amount, page_name):
+    item = ""
+    for i in range(image_amount):
+
+        with open("data/content.json") as f:
+            data = json.load(f)
+
+        for char in data.get(page_name, []):
+
+            type_ = char.get("type", None)
+            if type_ == "ImageGalreyModule": 
+                image = char.get(f"img-{i}", None)
+                item += f"""<img src="{image}">"""
+
+    module = f"""
+    <section>
+        <div></div>
+        <div><label class="code-text-design">img-4xl img-sze img-center</label></div>
+        <div class="img-galery">
+            {item}
+        </div>
+    </section>
+    """
+    return module
+
+def BlogModule(title, text, img, button, accordion_amount, image_amount, blog_amount, card_amount, page_name):
+    item = ""
+    for i in range(blog_amount):
+
+        with open("data/content.json") as f:
+            data = json.load(f)
+
+        for char in data.get(page_name, []):
+
+            type_ = char.get("type", None)
+            if type_ == "BlogModule": 
+                title = char.get(f"title-{i}", None)
+                text = char.get(f"text-{i}", None)
+                item += f"""
+                <div><h1 class="title-design-2">{title}</h1></div>
+                <div><label class="code-text-design">text-base text-white</label></div>
+                <div><label class="text-design-2">{text}</label></div>
+                
+                <div></div>
+                """
+
+    module = f"""
+    <section style="margin-top: 100px; margin-bottom: 100px;">
+        {item}
+    </section>
+    """
+    return module
+
+def CardModule(title, text, img, button, accordion_amount, image_amount, blog_amount, card_amount, page_name):
+    item = ""
+    for i in range(card_amount):
+
+        with open("data/content.json") as f:
+            data = json.load(f)
+
+        for char in data.get(page_name, []):
+
+            type_ = char.get("type", None)
+            if type_ == "CardModule": 
+                card_image = char.get(f"img-{i}", None)
+                card_title = char.get(f"title-{i}", None)
+                card_text = char.get(f"text-{i}", None)
+                card_rank = char.get(f"rank-{i}", None)
+
+                item += f"""
+                <div class="card">
+                    <img src="{card_image}">
+                    <h2 class="title-design-2">{card_title}<label class="side-text rank-{card_rank}">{card_rank}</label></h2>
+                    <label class="text-design-2">{card_text}</label>
+                </div>
+                """
+
+    module = f"""
+    <section style="margin-top: 100px; margin-bottom: 100px;">
+        <div><label class="code-text-design">text-4x2 tracking-tighter text-balance</label></div>
+        <div><h1 class="title-design-2">{title}</h1></div>
+        <div><label class="code-text-design">text-base text-white text-size text-5x1</label></div>
+        <div><label class="text-design-2">{text}</label></div>
+        
+        <div class="cards-frame">
+            {item}
+        </div>
+    </section>
     """
     return module
